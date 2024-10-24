d3a2d9926c48c239d07c347de54a87288fb710d1
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-10-24 22:36:52 +0530

    Update: scroller complete

--- src/components/sections/Scroller.js
+++ src/components/sections/Scroller.js
@@ -1,68 +1,64 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { Draggable } from "gsap/dist/Draggable";
 
 const Scroller = () => {
-  const timelineRef = useRef(null);
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    gsap.registerPlugin(Draggable, ScrollTrigger);
-    if (timelineRef.current) {
-      Draggable.create(timelineRef.current, {
-        type: "x",
-        bounds: ".timeline",
-        edgeResistance: 0.05, // Lower values increase drag sensitivity
-        inertia: true, // Enable inertia for smoother dragging
-        throwProps: true, // Use ThrowProps plugin if available for momentum-based dragging
-        onDrag: function () {
-          console.log("Dragging...");
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-430vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "5000 top",
+          scrub: 0.5,
+          pin: true,
         },
-        onDragEnd: function () {
-          console.log("Drag ended.");
-        },
-      });
-      
-    }
+      }
+    );
+
+    return () => {
+      pin.kill();
+    };
   }, []);
 
   return (
-    <div className="w-full h-screen">
-      <div className="flex w-full p-14 z-20">
-        <div className="w-1/6"></div>
-        <div className=" w-5/5 flex justify-end">
-          <p className="text-accent text-[8rem] font-semibold leading-[8rem] custom-font">
-            {" "}
+    <div className="scroller-container overflow-hidden">
+      <div ref={triggerRef}>
+        <div className="blend-css scroller-fixed-text fixed top-10 right-0 w-[80%] flex justify-center z-20 leading-[8rem]">
+          <p className="custom-font text-[7.5rem] font-bold text-accent p-3 rounded-md">
             We make scrolling worth it.
           </p>
         </div>
-      </div>
 
-      <div className="relative h-[100vh] overflow-x-auto -mt-[31vh] z-10">
-        <div
-          className="absolute top-0 left-0 w-[500vw] h-[80vh] flex flex-nowrap timeline"
-          ref={timelineRef}
-        >
-          <section  className="relative w-[20vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0">
+        <div ref={sectionRef} className="h-[100vh] w-[530vw] flex relative">
+          <section className="w-[30vw] flex justify-around items-center"></section>
 
-          </section>
-          <section
-            id="section-1"
-            className="relative w-[100vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0"
-          >
-            <div className="img img-1 w-[50vw] h-full">
+          <section className="w-[100vw] flex justify-around items-end py-10">
+            <div className="img img-1 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-1.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-2 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-2.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-3 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-3.png"
                 className="w-full h-full object-cover rounded-3xl"
@@ -70,23 +66,20 @@ const Scroller = () => {
             </div>
           </section>
 
-          <section
-            id="section-2"
-            className="relative w-[100vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0"
-          >
-            <div className="img img-1 w-[50vw] h-full ">
+          <section className="w-[100vw] flex justify-around items-end py-10">
+            <div className="img img-1 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-2.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-2 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-1.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-3 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-3.png"
                 className="w-full h-full object-cover rounded-3xl"
@@ -94,37 +87,29 @@ const Scroller = () => {
             </div>
           </section>
 
-          <section
-            id="section-3"
-            className="relative w-[100vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0"
-          >
-            <h1>swjhbdj diuwbd bdiw ddiwqqnd diwendx</h1>
-            <p>
-              swjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd
-              diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw
-              ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd
-              bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj
-              diuwbd bdiw ddiwqqnd diwendx
+          <section className="w-[100vw] flex justify-center items-end py-10 flex-col ">
+            <h1 className="text-2xl font-bold mb-4">Text Section</h1>
+            <p className="text-lg w-[60%] text-center">
+              This is a sample paragraph text. The height and width are adjusted
+              for consistent design across the sections, providing a
+              well-balanced layout.
             </p>
           </section>
 
-          <section
-            id="section-4"
-            className="relative w-[100vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0"
-          >
-            <div className="img img-1 w-[50vw] h-full">
+          <section className="w-[100vw] flex justify-around items-end py-10">
+            <div className="img img-1 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-3.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-2 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-5.png"
                 className="w-full h-full object-cover rounded-3xl"
               />
             </div>
-            <div className="img img-1 w-[50vw] h-full">
+            <div className="img img-3 w-[30vw] h-[70vh]">
               <img
                 src="/images/banner-1.png"
                 className="w-full h-full object-cover rounded-3xl"
@@ -132,17 +117,11 @@ const Scroller = () => {
             </div>
           </section>
 
-          <section
-            id="section-5"
-            className="relative w-[100vw] h-full p-[6em_2em_0_2em] flex gap-8 flex-shrink-0"
-          >
-            <h1>swjhbdj diuwbd bdiw ddiwqqnd diwendx</h1>
-            <p>
-              swjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd
-              diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw
-              ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj diuwbd
-              bdiw ddiwqqnd diwendxswjhbdj diuwbd bdiw ddiwqqnd diwendxswjhbdj
-              diuwbd bdiw ddiwqqnd diwendx
+          <section className="w-[100vw] flex justify-center items-end py-10 flex-col">
+            <h1 className="text-2xl font-bold mb-4">Text Section</h1>
+            <p className="text-lg w-[60%] text-center">
+              This is a sample paragraph text. The last section follows a
+              similar structure to maintain consistency and balance.
             </p>
           </section>
         </div>
