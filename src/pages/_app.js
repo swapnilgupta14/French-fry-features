c20c70e097c3ef0a68b3e73ad98e1ed7804cbf26
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-11-21 18:48:11 +0530

    upadate

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
 import CustomCursor from "../components/common/CustomCursor";
-// import Template from "./tempelate";
-import LoadingScreen from "./LoadingScreen";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <LoadingScreen />
       <CustomCursor />
       <Component {...pageProps} />
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,9 @@
-import { useState, useEffect } from "react";
-import { gsap } from "gsap";
 import Homepage from "./homepage";
 import UseLenis from "../utils/customHooks/UseLenis";
 
 export default function Home() {
   UseLenis();
-  const [isLoading, setIsLoading] = useState(false);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [progress, setProgress] = useState(0);
-
-  // useEffect(() => {
-  //   // Animate the loading progress
-  //   const tl = gsap.timeline({
-  //     onComplete: () => setIsLoading(false),
-  //   });
-
-  //   tl.to(
-  //     {},
-  //     {
-  //       duration: 3,
-  //       onUpdate: () => {
-  //         setProgress((tl.progress() * 100).toFixed(0));
-  //       },
-  //     }
-  //   );
-  // }, []);
-
-  return <>{isLoading ? <Loader progress={progress} /> : <Homepage />}</>;
-}
-
-function Loader({ progress }) {
-  useEffect(() => {
-    gsap.fromTo(
-      ".loader",
-      { opacity: 1, scale: 1 },
-      { opacity: 0, scale: 0.9, delay: 2, duration: 0.5, ease: "power3.out" }
-    );
-  }, []);
-
   return (
-    <div className="loader fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black text-white z-50">
-      <div className="text-center">
-        <div className="text-4xl font-bold">{progress}%</div>
-        <p className="text-lg mt-2">Loading...</p>
-      </div>
-    </div>
-  );
+    <Homepage/>
+  )
 }
